7f5153f5d5d458446ddd83ac367f8e79df6458ba
Damoiskii <moimyazz@gmail.com>
2026-09-14 21:07:14 -0500

    feat: add initial components and utility files for slider puzzle

---
+++ src/components/Draggable.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Draggable = () => {
+  return (
+    <View>
+      <Text>Draggable</Text>
+    </View>
+  )
+}
+
+export default Draggable
